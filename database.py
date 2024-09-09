e89a7ec2a0b346cfb7a29f7ca52bb7bfdb307ddf
Muhammad Faizan <muhammadfaizanlite@gmail.com>
2024-09-09 19:43:57 +0500

    update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 DB_USERNAME = os.environ.get('DB_USERNAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
-DB_HOSTNAME = os.environ.get('DB_HOSTNAME', '127.0.0.1')  # Default to 127.0.0.1
+DB_HOSTNAME = os.environ.get('DB_HOSTNAME')  # Default to 127.0.0.1
 DB_NAME = os.environ.get('DB_NAME', 'careersoncloud')
 
 if not DB_USERNAME or not DB_PASSWORD:
